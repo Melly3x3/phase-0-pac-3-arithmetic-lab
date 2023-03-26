0b4cb57c5b205b521d2ac436f11f554dea850dc8
Melvin <jmelvin1134@gmail.com>
2023-03-25 22:12:07 -0400

    arith lab

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function add(a, b) {
  function divide(a, b) {
    return a / b
  }
- function inc(a) {
-   return a += 1
+ function inc(n) {
+   return n + 1
  }
- function dec(a) {
-   return a -= 1
+ function dec(n) {
+   return n - 1
  }
  function makeInt(n) {
-   return parseInt(n,10)
+   return parseInt(n, 10)
  }
  function preserveDecimal(n) {
    return parseFloat(n)
